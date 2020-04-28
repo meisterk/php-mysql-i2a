f2364ecc55cf052740c0bc6ce05182abde8d5ba2
Franz Kohnle <franz@kohnlehome.de>
2020-04-28 12:05:36 +0200

    Add comments

--- create-database.php
+++ create-database.php
@@ -1,15 +1,17 @@
 <?php
 
 
-
+// connect to MySQL as MySQL-Root
 $rootpassword = 'mysql';
 $pdo = new PDO(
     "mysql:host=localhost",
     'root',
     $rootpassword
 );
+// show errors
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+// create database
 echo 'Try to create database ...';
 $sql = "CREATE DATABASE testdb";
 $pdo->exec($sql);
