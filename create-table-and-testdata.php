2f1f89eb28ef764df84b4fe1d91629f2eb3578e8
Franz Kohnle <franz@kohnlehome.de>
2020-05-05 11:24:27 +0200

    create table

--- create-table-and-testdata.php
+++ create-table-and-testdata.php
@@ -11,3 +11,15 @@ $pdo = new PDO(
 );
 // show errors
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// create table
+echo 'Try to create table ...<br>';
+$sql = "DROP TABLE IF EXISTS schueler";
+$pdo->exec($sql);
+
+$sql = "CREATE TABLE schueler (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    vorname CHAR(20),
+    nachname CHAR(20)
+)";
+$pdo->exec($sql);
