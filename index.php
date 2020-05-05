cda61c3bd87fba67c58b717843d223691dfc11c2
Franz Kohnle <franz@kohnlehome.de>
2020-05-05 11:41:53 +0200

    Add html

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Schülerverwaltung</title>
+</head>
+
+<body>
+    <h1>Schülerverwaltung</h1>
+    <table>
+        <tr>
+            <th>Vorname</th>
+            <th>Nachname</th>
+        </tr>
+
+        <tr>
+            <td>Anna</td>
+            <td>Arm</td>
+        </tr>
+        <tr>
+            <td>Anna</td>
+            <td>Arm</td>
+        </tr>
+        <tr>
+            <td>Anna</td>
+            <td>Arm</td>
+        </tr>
+    </table>
+</body>
+
+</html>
